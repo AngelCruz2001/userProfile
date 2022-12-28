8e3212a1a68cc4967899592d19df672853161cda
angi_ed01@hotmail.com <angi_ed01@hotmail.com>
2022-12-28 09:02:56 -0600

    redirect root to users

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,17 +1,23 @@
 import { Sidebar, Navbar, Card } from "components";
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect, useNavigate } from "react-router-dom";
 import styles from "styles/modules/Root.module.scss";
 import { useEffect } from "react";
 import { data } from "helpers/data";
 
 export const Root = () => {
+  let navigate = useNavigate();
   useEffect(() => {
     // Saving in local storage the information to simulate the database
-    console.log("aSDF");
     if (!localStorage.getItem("business"))
       localStorage.setItem("business", JSON.stringify(data.business));
     if (!localStorage.getItem("users"))
       localStorage.setItem("users", JSON.stringify(data.users));
+  }, []);
+
+  useEffect(() => {
+    console.log("redirect");
+    // Redirect to users page
+    navigate("/users");
   }, []);
 
   return (
