0d7ca506ae3490b02d185c757496ee68415ed536
angi_ed01@hotmail.com <angi_ed01@hotmail.com>
2022-12-28 09:08:26 -0600

    toaster fixed size

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter />
-      <Toaster />
+      <Toaster
+        toastOptions={{
+          className: "toaster",
+        }}
+      />
     </Provider>
   </React.StrictMode>
 );
